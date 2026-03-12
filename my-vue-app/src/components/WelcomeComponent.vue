988bfc96b850277c6a777eb9c3edb737e2960dbe
展金昊 <2251498846@qq.com>
2026-03-12 16:56:21 +0800

    商品查询表格

--- my-vue-app/src/components/WelcomeComponent.vue
+++ my-vue-app/src/components/WelcomeComponent.vue
@@ -6,10 +6,35 @@
     <router-link to="/welcome/parent">父组件</router-link> | 
     <router-link to="/welcome/child">子组件</router-link>
   </nav>
-    <button class="Login" @click="testButton">报错退出登录</button>
+  <router-view />
+    <button class="Login" @click="testButton">报错退出</button>
+    <div>
+      <label>商品ID：</label>
+      <input v-model="shopId">
+    </div>
+      <button class="Login" @click="queryById">按照ID检索商品</button>
+      <table v-if="responseGoods" class="goods-table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>名称</th>
+          <th>价格</th>
+          <th>库存</th>
+          <th>描述</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{{ responseGoods.goodsId }}</td>
+          <td>{{ responseGoods.goodsName }}</td>
+          <td>{{ responseGoods.goodsPrice }}</td>
+          <td>{{ responseGoods.goodsStock }}</td>
+          <td>{{ responseGoods.goodsDescription }}</td>
+        </tr>
+      </tbody>
+    </table>
       <p v-if="error" style="color: red">{{ error }}</p>
       <p v-if="success" style="color: green">{{ success }}</p>
-  <router-view />
 </template>
 
 <script setup>
@@ -18,16 +43,39 @@ import request from '../commonUtils/commonRequest'
 
 const error = ref('')
 const success = ref('')
+const shopId = ref('')
+const responseGoods = ref(null)
 
 const testButton = async () => {
   try {
     // 发送 POST 请求到后端
-    const response = await request.post('http://localhost:8080/User/test', {
+    const response = await request.post('/user/test', {
     })
 
     if(response.data.isSuccess){
     error.value = ''
     success.value = 'Test successful'
+    }else{
+    error.value = response.data.message
+    success.value = ''
+    }
+
+  }catch (err) {
+    if (err.response) {
+      // 后端返回了错误状态码（如 401）
+      error.value = err.response?.data || '登录失败，请检查网络'
+    }
+  }
+}
+const queryById = async () => {
+  try {
+    // 发送 get 请求到后端
+    const response = await request.get(`http://localhost:8080/goods/${shopId.value}`)
+
+    if(response.data){
+    responseGoods.value = response.data
+    error.value = ''
+    success.value = 'getGoods successful'
     }else{
     error.value = response.data.message
     success.value = ''
@@ -52,6 +100,22 @@ const testButton = async () => {
 .Login:hover {
   background-color: rgb(119, 174, 226);
   color:rgb(0, 7, 12);
+}
+.goods-table {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 20px;
+}
+
+.goods-table th,
+.goods-table td {
+  border: 1px solid #ddd;
+  padding: 10px;
+  text-align: left;
+}
+
+.goods-table th {
+  background-color: #f4f4f4;
 }
 
 </style>
